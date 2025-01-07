d510749d9ae8b99092363362343c32be79288958
niraj551 <nirajbhusal2077@gmail.com>
2025-01-07 21:00:02 +0545

    Day 16 Completed

---
+++ 16-Day/JSON.js
@@ -0,0 +1,89 @@
+// Example
+const rect = `{
+  "rectangle" : {
+    "length":20
+    "breadth":20
+  }
+}`
+
+
+// json to object
+const usersText = `{
+  "users":[
+    {
+      "firstName":"Asabeneh",
+      "lastName":"Yetayeh",
+      "age":250,
+      "email":"asab@asb.com"
+    },
+    {
+      "firstName":"Alex",
+      "lastName":"James",
+      "age":25,
+      "email":"alex@alex.com"
+    },
+    {
+    "firstName":"Lidiya",
+    "lastName":"Tekle",
+    "age":28,
+    "email":"lidiya@lidiya.com"
+    }
+  ]
+  }`
+const usersObj = JSON.parse(usersText, undefined, 4)
+console.log(usersObj)
+
+
+// using reviver function with JSON.parse()
+const usersObj1 = JSON.parse(usersText, (key, value) => {
+  let newValue =
+    typeof value == 'string' && key != 'email' ? value.toUpperCase() : value
+  return newValue
+})
+console.log(usersObj1)
+
+
+// converting object to JSON
+const users = {
+  Alex: {
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+    age: 20,
+    isLoggedIn: false,
+    points: 30
+  },
+  Asab: {
+    email: 'asab@asab.com',
+    skills: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'Redux',
+      'MongoDB',
+      'Express',
+      'React',
+      'Node'
+    ],
+    age: 25,
+    isLoggedIn: false,
+    points: 50
+  }
+}
+const txt = JSON.stringify(users, undefined, 4)
+console.log(txt) // text means JSON- because json is a string form of an object.
+
+
+// using filter array with JSON.stringify
+const user = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+  country: 'Finland',
+  city: 'Helsinki',
+  email: 'alex@alex.com',
+  skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Python'],
+  age: 250,
+  isLoggedIn: false,
+  points: 30
+}
+const txt1 = JSON.stringify(user,['firstName', 'lastName', 'country', 'city', 'age'],4)
+console.log(txt1)
