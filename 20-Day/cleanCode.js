0ce75e7e7a9390cfdca64caecc2b20739a83eb1d
niraj551 <nirajbhusal2077@gmail.com>
2025-01-11 19:03:20 +0545

    Day 20 Completed

---
+++ 20-Day/cleanCode.js
@@ -0,0 +1,31 @@
+// Writing clean code
+
+// Need of style guide
+/*
+It does not matter in anyway you write you code as long as it running,
+however when you work in team of 10 or 20 or more developer on one project
+and on the same code base, the code will be messy and hard to manage if there
+is no any guidelines to follow.
+*/
+
+// Coding Conventions
+/*
+Naming and declaration rules for variables
+Naming and declaration rules for functions
+Rules for the use of white space, indentation, and comments
+Programming practices and principles
+*/
+
+// Conventions used in this challenge
+/*
+We used camelCase for variables and functions.
+All variable names start with a letter.
+We chose to use const for constants, arrays, objects and functions. In stead of double quote, we chose to use single quote or backtick. Single quote is becoming trendy.
+We also removed semicolons from our code but it is a matter of personal preference.
+Space around arithmetic operators, assignment operators and after comma
+Arrow function instead of function declaration
+Explicit return instead of implicit return if the function is one liner
+No trailing comma in the last value of an object
+We prefer this +=, -=, = /=, *= instead of the longer version
+When we use console.log() it is good to print with a tag string to identify from where the console is coming
+*/
