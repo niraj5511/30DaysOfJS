67075b63eff9bbb42026ff4301c27c2fa0a6b6cb
niraj551 <nirajbhusal2077@gmail.com>
2025-01-13 20:18:18 +0545

    Day 22 Completed

---
+++ 22-Day/DOM-2.js
@@ -0,0 +1,56 @@
+// DOM-Day 2
+
+// creating an element
+
+// let title = document.createElement("h1");
+// title.className = "title";
+// title.style.fontSize = "24px";
+// title.textContent = "Creating HTML element DOM Day 2";
+
+// creating multiple elements
+
+// let title;
+// for (let i = 0; i < 3; i++) {
+//   title = document.createElement("h1");
+//   title.className = "title";
+//   title.style.fontSize = "24px";
+//   title.textContent = i;
+//   console.log(title);
+// }
+
+// creating multiple elements and appending to parent element
+
+// let title;
+// for (let i = 0; i < 3; i++) {
+//   title = document.createElement("h1");
+//   title.className = "title";
+//   title.style.fontSize = "24px";
+//   title.textContent = i;
+//   document.body.appendChild(title);
+// }
+
+// removing child element from parent node
+{
+  /* <body>
+    <h1>Removing child Node</h1>
+    <h2>Asabeneh Yetayeh challenges in 2020</h1>
+    <ul>
+        <li>30DaysOfPython Challenge Done</li>
+        <li>30DaysOfJavaScript Challenge Done</li>
+        <li>30DaysOfReact Challenge Coming</li>
+        <li>30DaysOfFullStack Challenge Coming</li>
+        <li>30DaysOfDataAnalysis Challenge Coming</li>
+        <li>30DaysOfReactNative Challenge Coming</li>
+        <li>30DaysOfMachineLearning Challenge Coming</li>
+    </ul>
+
+    <script>
+        const ul = document.querySelector('ul')
+        const lists = document.querySelectorAll('li')
+        for (const list of lists) {
+            ul.removeChild(list)
+
+        }
+    </script>
+</body> */
+}
