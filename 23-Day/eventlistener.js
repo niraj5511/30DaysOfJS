40cfcf4452b6412127e50624b4e119cadda371d8
niraj551 <nirajbhusal2077@gmail.com>
2025-01-14 19:37:14 +0545

    Day 23 Completed

---
+++ 23-Day/eventlistener.js
@@ -0,0 +1,39 @@
+// event listeners
+
+// click event
+// const button = document.querySelector("button");
+// button.addEventListener("click", (e) => {
+//   console.log("e gives the event listener object:", e);
+//   console.log("e.target gives the selected element: ", e.target);
+//   console.log(
+//     "e.target.textContent gives content of selected element: ",
+//     e.target.textContent
+//   );
+// });
+
+// onclick event - can be added directly to HTML elements
+
+// double click event - "dblclick" instead of "click"
+
+// mouse enter event - "mouseenter"
+
+// getting value from an input element
+
+// const mass = document.querySelector("#mass");
+// const height = document.querySelector("#height");
+// const button = document.querySelector("button");
+// let bmi;
+// button.addEventListener("click", () => {
+//   bmi = mass.value / height.value ** 2;
+//   alert(`your bmi is ${bmi.toFixed(2)}`);
+//   console.log(bmi);
+// });
+
+// input event - "input"
+
+// p.textContent = e.target.value - to get data right away from the input field when the field is on focus
+
+// blur event - "blur" , when input field is not on focus
+
+// key press event - "keypress"
+// e.keyCode - to get the key code of pressed key on keyboard
