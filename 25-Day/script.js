cb7a6c8c6915cc04a743d60804bd836ca8daaba9
niraj551 <nirajbhusal2077@gmail.com>
2025-01-16 21:37:50 +0545

    Some Basics Completed

---
+++ 25-Day/script.js
@@ -0,0 +1,21 @@
+const population = document.querySelector(".population")
+const languages = document.querySelector(".language")
+const body = document.querySelector("body")
+const bar = document.querySelector(".bar")
+const barContainer = document.querySelector(".bar-container")
+
+let bar1,barValue1,barNumber1
+population.addEventListener("click",()=>{
+
+  // bar.style.display = "flex"
+  // bar1 = document.createElement("div")
+  // console.log(bar1)
+  // bar1.className="bar1"
+  // barValue1 = document.createElement("div")
+  // console.log(barValue1)
+  // barValue1.className="barValue-1"
+  // barValue1.textContent=" "
+
+  // // body.appendChild(bar1)
+  // body.appendChild(barValue1)
+})
